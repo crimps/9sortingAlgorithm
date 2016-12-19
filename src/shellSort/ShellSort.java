0591163334d05c3dc19e553d690f1062e8ff7ab1
damonMPR <woolpack.cwz@gmail.com>
2016-12-19 13:03:08 +0800

    希尔排序

---
+++ src/shellSort/ShellSort.java
@@ -0,0 +1,28 @@
+/**
+ * @(#)shellSort.ShellSort.java Copyright (c) 2014-2018 crimps
+ */
+package shellSort;
+
+/**
+ *
+ * @author crimps
+ * @version 1.0  16/12/14
+ * @modified crimps  16/12/14  <创建>
+ */
+
+/**
+ * 希尔排序的实质就是分组插入排序，该方法又称缩小增量排序
+ * 先将整个待排元素序列分割成若干个子序列（由相隔某个“增量”的元素组成的）分别进行直接插入排序，
+ * 然后依次缩减增量再进行排序，待整个序列中的元素基本有序（增量足够小）时，再对全体元素进行一次直接插入排序。
+ * 因为直接插入排序在元素基本有序的情况下（接近最好情况），效率是很高的，
+ * 因此希尔排序在时间效率上有较大提高
+ */
+public class ShellSort {
+    public void shellSort(int[] nums) {
+        for(int increment = nums.length/2; increment > 0; increment /=2) {
+            for(int i = 0; i + 1 < nums.length; i++) {
+
+            }
+        }
+    }
+}
